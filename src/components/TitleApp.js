03084900677e53a7b5e4c8ce986c758ebcd83c39
LeodanCondori7000 <leodccc7@gmail.com>
2023-08-21 21:09:32 -0500

    components folder created and styles added

---
+++ src/components/TitleApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TitleApp extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className='main-title'>Popular Products</h1>
+        <hr />
+      </header>
+    );
+  }
+}
